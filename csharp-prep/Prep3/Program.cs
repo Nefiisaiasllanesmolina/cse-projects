a6e6056e004aa308d8d62f1719779532df2cb30a
nefillanes <nefillanes95@gmail.com>
2024-11-08 13:24:38 -0600

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,28 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        int magicNumber = 3;
         int response = 0;
+        int counter = 0;
+        string keepPlaying = "yes";
 
-        while (magicNumber != response)
+        while (keepPlaying == "yes")
         {
-            Console.Write("What is your guess? ");
-            string answer = Console.ReadLine();
-            int guess = int.Parse(answer);
+            counter = 0;
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 10);
+            while (magicNumber != response)
+            {
+                Console.Write("What is your guess? ");
+                string answer = Console.ReadLine();
+                int guess = int.Parse(answer);
+                counter = counter + 1;
 
-            if (guess > magicNumber)
-            {
-                Console.WriteLine("Your guess is higher than magic number");
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Your guess is higher than magic number");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Your guess is lower than magic number");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it {counter}");
+                }
+                response = guess;
             }
-            else if (guess < magicNumber)
-            {
-                Console.WriteLine("Your guess is lower than magic number");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it");
-            }
-            response = guess;
+
+        Console.WriteLine("Do you want to play again? ");
+        string yesorno = Console.ReadLine();
+        response = 0;
+        keepPlaying = yesorno;
         }
     }
 }
