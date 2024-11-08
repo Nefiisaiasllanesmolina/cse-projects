37e0ebee073a8d9ad773743d5c460db73c2bf119
nefillanes <nefillanes95@gmail.com>
2024-11-08 12:54:35 -0600

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = 3;
+        int response = 0;
+
+        while (magicNumber != response)
+        {
+            Console.Write("What is your guess? ");
+            string answer = Console.ReadLine();
+            int guess = int.Parse(answer);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Your guess is higher than magic number");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Your guess is lower than magic number");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it");
+            }
+            response = guess;
+        }
     }
 }
