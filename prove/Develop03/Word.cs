439350b514ab79c376179f2f6c63dc534602f617
nefillanes <nefillanes95@gmail.com>
2024-12-14 10:03:08 -0600

    Scripture-Memorizer

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,4 +1,29 @@
 public class Word{
     private string _text;
     private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    /*public bool IsHidden()
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+
+    }*/
 }
