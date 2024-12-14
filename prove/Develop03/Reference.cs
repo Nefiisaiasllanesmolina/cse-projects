cb4bd9f2a350029631332c530f8fe93d0890e00c
nefillanes <nefillanes95@gmail.com>
2024-12-13 18:03:34 -0600

    Scripture-Memorizer

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -3,4 +3,9 @@ public class Reference{
     int _chapter;
     int _verse;
     int _endVerse;
+
+    public string References(){
+        string _reference = _book + _chapter + ":" + _verse + "-" + _endVerse;
+        return _reference;
+    }
 }
