57de2caead299562bcf992479785d85f23c76bef
nefillanes <nefillanes95@gmail.com>
2024-12-17 10:59:30 -0600

    Mindfulness-Program

---
+++ prove/Develop05/ListingActivity.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+
+public class ListingActivity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+    public ListingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        List<string> hola = new List<string>();
+        return hola;
+    }
+}
