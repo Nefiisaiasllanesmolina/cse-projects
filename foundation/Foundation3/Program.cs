bdf956ebde86cf179bc0e67ccf1a3a4077d5a408
nefillanes <nefillanes95@gmail.com>
2024-12-19 10:02:41 -0600

    Foundation 3

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        List<Activity> activitiesList = new List<Activity>();
+
+        Running running = new Running(20, 3);
+        activitiesList.Add(running);
+        Cycling cycling = new Cycling(25, 4);
+        activitiesList.Add(cycling);
+        Swimming swimming = new Swimming(30, 30);
+        activitiesList.Add(swimming);
+
+        foreach (Activity activity in activitiesList)
+        {
+            activity.DisplaySummary();
+        }
     }
 }
